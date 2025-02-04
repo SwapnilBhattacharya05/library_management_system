5b77349d2bf6b409adc088afbc7d39e2fc4a5b4b
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-02-04 19:16:19 +0530

    test commit to check if error only on localhost or production site

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 // SINCE IT IS A FORM AND MANAGING A LOT OF THINGS
-import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   DefaultValues,
@@ -10,12 +9,12 @@ import {
   useForm,
   UseFormReturn,
 } from "react-hook-form";
-import { z, ZodType } from "zod";
+import { ZodType } from "zod";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -65,15 +64,15 @@ const AuthForm = <T extends FieldValues>({
       toast({
         title: "Success",
         description: isSignIn
-          ? "You have successfully signed in"
-          : "successfully signed up",
+          ? "You have successfully signed in."
+          : "You have successfully signed up.",
       });
 
       router.push("/");
     } else {
       toast({
-        title: `"Error ${isSignIn ? "signing in" : "signing up"}`,
-        description: result.error ?? "Something went wrong",
+        title: `Error ${isSignIn ? "Signing In" : "Signing Up"}`,
+        description: result.error ?? "An Error Occurred.",
         variant: "destructive",
       });
     }
@@ -82,7 +81,7 @@ const AuthForm = <T extends FieldValues>({
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold text-white">
-        {isSignIn ? "Welcome Back to BookWise" : "Create your Library Account"}
+        {isSignIn ? "Welcome back to BookWise" : "Create your Library Account"}
       </h1>
       <p className="text-light-100">
         {isSignIn
@@ -92,7 +91,7 @@ const AuthForm = <T extends FieldValues>({
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(handleSubmit)}
-          className="space-y-6 w-full"
+          className="w-full space-y-6"
         >
           {Object.keys(defaultValues).map((field) => (
             <FormField
@@ -138,12 +137,12 @@ const AuthForm = <T extends FieldValues>({
         </form>
       </Form>
       <p className="text-center text-base font-medium">
-        {isSignIn ? "New to Bookwise? " : "Already have an account? "}
+        {isSignIn ? "New to BookWise? " : "Already have an account? "}
         <Link
-          className="font-bold text-primary"
           href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary"
         >
-          {isSignIn ? "Create an Account" : "Sign In"}
+          {isSignIn ? "Create an account" : "Sign in"}
         </Link>
       </p>
     </div>
